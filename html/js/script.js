4914b0a16a00c5894e1f3db4be495500dcf06406
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-05-21 17:06:44 -0300

    estudo js

--- html/js/script.js
+++ html/js/script.js
@@ -2,20 +2,17 @@ let input = document.getElementById("inputTarefa");
 let btnAdd = document.getElementById("btn-add");
 let main = document.getElementById("areaLista");
 
-
 function addTarefa(){
     //pegar o valor digitado no input
     let valorInput = input.value;
-
     // se nao for vazio, nem nulo, nem indefinido
-
     if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
         // HTML QUE VAI SER INSERIDO
         let novoItem = `        
         <div class="item">     
             <div class="item-icone">
                 <i class="fa-regular fa-circle"></i>
-                Primeira tarefa adicionada
+            ${valorInput}
             </div>
             <div class="item-nome">
             </div>
@@ -24,8 +21,13 @@ function addTarefa(){
             </div>
         </div> 
 `
-
+        // ADICIONAR NOVO ITEM MAIN
         main.innerHTML += novoItem;
+        // ZERAR OS CAMPO di unput
+        input.value = "";
+        //MANTER O CAMPO INPUT FOCADO
+        input.focus();
+
         };
     };
 
