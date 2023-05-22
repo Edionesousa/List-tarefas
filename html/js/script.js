c58c566379f9ad26ba6e591c9ca2d7f883790613
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-05-22 13:32:53 -0300

    estudo js

--- html/js/script.js
+++ html/js/script.js
@@ -1,7 +1,9 @@
+
+let contador = 0;
+
 let input = document.getElementById("inputTarefa");
 let btnAdd = document.getElementById("btn-add");
 let main = document.getElementById("areaLista");
-let buttonDelet = document.getElementById("button");
 
 function addTarefa(){
     // PEGAR O VALOR DIGITADO NO INPUT
@@ -9,8 +11,9 @@ function addTarefa(){
     // SE NAO FOR VAZIO,          NAO FOR NULO,              NAO FOR INDEFINIDO
     if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
         // HTML QUE VAI SER INSERIDO
+        ++contador;
         let novoItem = `        
-        <div class="item">     
+        <div id ="${contador}" class="item">     
             <div class="item-icone">
                 <i class="fa-regular fa-circle"></i>
             ${valorInput}
@@ -18,24 +21,26 @@ function addTarefa(){
             <div class="item-nome">
             </div>
             <div class="item-botao button">
-                <button onclick("delet")id="deletar"><i class="fa-solid fa-trash"></i> Deletar</button>
+                <button onclick="deletar(${contador})" id="deletar"><i class="fa-solid fa-trash"></i> Deletar</button>
             </div>
         </div> 
-`
+        `
         // ADICIONAR NOVO ITEM HTML MAIN NA TELA
         main.innerHTML += novoItem;
         // ZERAR OS CAMPO DO INPUT
         input.value = "";
         // MANTER O CAMPO INPUT FOCADO
         input.focus();
-
         };
     };
-
-
+    function deletar(){
+        let tarefa = document.getElementById(id)
+        tarefa.remove();
+    }
     input.addEventListener("keyup", function(event){
         // ADD USANDO TELCA ENTER (13)
         if(event.keyCode === 13){
+            let key = event;
             event.preventDefault();
             addTarefa();
         }
