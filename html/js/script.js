784f26c84660fd7a157b1951b9803138b232fa47
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-05-28 15:46:02 -0300

    estudo js

--- html/js/script.js
+++ html/js/script.js
@@ -1,54 +1,62 @@
 
-let contador =0;
+let contador = 0;
 //PEGAR DADOS DO INPUT
 let input = document.getElementById("inputTarefa");
-
 //PARTE CORPO DO CODIGO
 let main = document.getElementById("areaLista");
-
     //ADICIONAR DADOS DO INPUT
 let btnAdd = document.getElementById("btn-add")
     btnAdd.addEventListener("click", addTarefa);
-
-
-
-    //DELETAR HTML INGETADO
-let deletar = document.getElementById("deletar")
-
     // FUNÇAO VAI ADICIONAR O HTML AO CORPO
-function addTarefa(item){
-    let valorInput = input.value; //PEGAR O VALOR DIGITADO NO INPUT
 
-    //SE NAO FOR VAZIO, NEM NULO, NEM INDEFINIDO.
+    function addTarefa(){
+        let valorInput = input.value; //PEGAR O VALOR DIGITADO NO INPUT
+        //SE NAO FOR VAZIO, NEM NULO, NEM INDEFINIDO.
     if((valorInput !=="")&&(valorInput !== null)&&(valorInput !== undefined)){
-        let novoIten = 
-    `<div class="item">
-    <div class="item-icone">
-        <i class="mdi mdi-circle-outline"></i>
-    </div>
-    <div class="item-nome">
+        ++contador;
+        let novoIten = `<div id="${contador}"class="item">
+        <div onclick ="marcarTarefa(${contador})" class="item-icone">
+        <i id="icone_${contador}"class="mdi mdi-circle-outline"></i>
+        </div>
+        <div onclick ="marcarTarefa(${contador})" class="item-nome">
         ${valorInput}
-    </div>
+        </div>
     <div class="item-botao">
-        <button onclick="delete()"id="delete" class="delete">
+        <button onclick="deletar(${contador})"class="delete" id="delete">
             Deletar <i class="mdi mdi-delete"></i>
         </button>
     </div>`;
-
     //ADICIONAR NOVO ITEM DO MAIN
     main.innerHTML += novoIten;
-
     //ZERAR OS CAPOS
     input.value = "";
     input.focus();
+    
     }
-
-
+}
+    function marcarTarefa(id){
+    const item =  document.getElementById(id);
+        var classe = item.getAttribute('class');
+    console.log(classe);
+        if(classe == "item"){
+    item.classList.add("clicado");
+    var icone = document.getElementById('icone_' + id);
+    icone.classList.remove('mdi-circle-outline');
+    icone.classList.add('mdi-check-circle')
+    item.parentNode.appendChild(item);
+    }else{
+        item.classList.remove('clicado');
+    var icone = document.getElementById('icone_' + id);
+    icone.classList.add('mdi-circle-outline')
     }
+}
 input.addEventListener("keyup", function(e){
     if(e.keyCode === 13){
         e.preventDefault();
         btnAdd.click();
     }
 })
-
+function deletar(id){
+    const tarefa = document.getElementById(id);
+    tarefa.remove();
+}
